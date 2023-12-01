16a2796107fa25a4d77318001fd372fb263bf9c3
Anne Hastings <anne.miriam@gmail.com>
2023-11-30 21:31:03 -0700

    complete lab

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
+const API = "http://localhost:3001/stocks"
+
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [sortBy, setSortBy] = useState("Alphabetically")
+  const [filterBy, setFilterBy] = useState("Tech")
+
+  useEffect(() => {
+    fetch(API)
+    .then(r=>r.json())
+    .then(setStocks)
+  }, [])
+
+  function handleAddStock(stockToAdd) {
+    const stockInPortfolio = portfolio
+      .find(stock => stock.id === stockToAdd.id)
+    if (!stockInPortfolio){
+      setPortfolio([...portfolio, stockToAdd])
+    }
+  }
+
+  function handleRmoveStock(stockToRemove) {
+    setPortfolio((portfolio) =>
+      portfolio.filter(stock => stock.id !== stockToRemove.id)
+    )
+  }
+
+  const sortStocks = [...stocks].sort((stock1, stock2) => {
+    if (sortBy === "Alphabetically") {
+      return stock1.name.localeCompare(stock2.name);
+    } else {
+      return stock1.price - stock2.price;
+    }
+  })
+
+  const filterStocks = sortStocks.filter((stock) => 
+    stock.type === filterBy)
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar filterBy={filterBy} onChangeFilter={setFilterBy} sortBy={sortBy} onChangeSort={setSortBy} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filterStocks} onAddStock={handleAddStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={portfolio} onRemoveStock={handleRmoveStock} />
         </div>
       </div>
     </div>
